4e3113f813e78ab07539851a291f1096c281e23b
mupied <mhmmadd.mufid@gmail.com>
2024-11-30 16:05:13 +0700

    first-commit

---
+++ database/migrations/2024_11_30_074043_create_materials_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('material_title');
+            $table->string('material_image')->nullable();
+            $table->string('description');
+            $table->unsignedBigInteger('id_teacher')->references('id')->on('roles')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materials');
+    }
+};
---
+++ database/migrations/2024_11_30_074129_create_answers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_question')->references('id')->on('questions')->nullable();
+            $table->string('choices');
+            $table->integer('correctAnswer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
